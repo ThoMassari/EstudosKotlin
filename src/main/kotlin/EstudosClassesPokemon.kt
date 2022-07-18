878fd0bc90d9097e5aa9f05e30eb1d94c728c995
ThoMassari <thomazmassari@hotmail.com>
2022-07-18 10:36:13 -0300

    inclusão de defesa

--- src/main/kotlin/EstudosClassesPokemon.kt
+++ src/main/kotlin/EstudosClassesPokemon.kt
@@ -1,4 +1,4 @@
-class Pokemon (var nome: String, var tipo: String, var vida: Int, var ataque: Int){
+class Pokemon (var nome: String, var tipo: String, var vida: Int, var ataque: Int, var defesa: Int){
     //ar nome = ""
     //var tipo = ""
     //var vida = 10
@@ -6,13 +6,13 @@ class Pokemon (var nome: String, var tipo: String, var vida: Int, var ataque: In
 
    fun ataque(ataque: Int, pokemonatacado: Pokemon){
 
-       var pokemon1 = Pokemon("Charmander", "Fogo", 20, 4)
-       var pokemon2 = Pokemon ("Bubassauro", "Planta", 22, 3)
+       var pokemon1 = Pokemon("Charmander", "Fogo", 20, 4,2)
+       var pokemon2 = Pokemon ("Bubassauro", "Planta", 22, 3, 1)
 
        println("OH MEU DEUS, " + pokemon1.nome + " está atacando " + pokemon2.nome + "com sua força total." )
        println("${pokemon2.nome} possuia ${pokemonatacado.vida} pontos de vida.")
-       pokemonatacado.vida -= ataque //vida = vida - ataque
-       println("Após esse ataque, ${pokemon2.nome} ficou com ${pokemonatacado.vida} pontos de via.")
+       pokemonatacado.defesa -= ataque - pokemonatacado.vida //vida = vida - ataque
+       println("Após esse ataque, ${pokemon2.nome} ficou com ${pokemonatacado.defesa} pontos de via.")
    }
 
 
@@ -20,8 +20,8 @@ class Pokemon (var nome: String, var tipo: String, var vida: Int, var ataque: In
 
 fun main () {
 
-    var pokemon1 = Pokemon("Charmander", "Fogo", 20, 4)
-    var pokemon2 = Pokemon ("Bubassauro", "Planta", 22, 3)
+    var pokemon1 = Pokemon("Charmander", "Fogo", 20, 4, 2)
+    var pokemon2 = Pokemon ("Bubassauro", "Planta", 22, 3, 1)
 
     pokemon1.ataque(pokemon1.ataque, pokemon2)
 
